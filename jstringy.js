21183d559936f7aa1705f9bd56ae899feea07342
chicagocomputerclasses <info@chicagocomputerclasses.com>
2016-01-09 20:13:37 -0600

    Update jstringy.js

--- jstringy.js
+++ jstringy.js
@@ -69,10 +69,37 @@
     }
 
 
+
+    // private reverse function
+    // by Mathias Bynens <https://mathiasbynens.be/>
+    // https://github.com/mathiasbynens/esrever
+    var reverse = function (string) {
+
+        var regexSymbolWithCombiningMarks = /([\0-\u02FF\u0370-\u1AAF\u1B00-\u1DBF\u1E00-\u20CF\u2100-\uD7FF\uE000-\uFE1F\uFE30-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])([\u0300-\u036F\u1AB0-\u1AFF\u1DC0-\u1DFF\u20D0-\u20FF\uFE20-\uFE2F]+)/g;
+        var regexSurrogatePair = /([\uD800-\uDBFF])([\uDC00-\uDFFF])/g;
+        // Step 1: deal with combining marks and astral symbols (surrogate pairs)
+        // Swap symbols with their combining marks so the combining marks go first
+        string = string.replace(regexSymbolWithCombiningMarks, function ($0, $1, $2) {
+                // Reverse the combining marks so they will end up in the same order
+                // later on (after another round of reversing)
+                return reverse($2) + $1;
+            })
+            // Swap high and low surrogates so the low surrogates go first
+            .replace(regexSurrogatePair, '$2$1');
+        // Step 2: reverse the code units in the string
+        var result = '';
+        var index = string.length;
+        while (index--) {
+            result += string.charAt(index);
+        }
+        return result;
+    };
+
+    // END private reverse function
+
+
     jInit.prototype.reversed = function () {
-        var s = this.val;
-        for (var i = s.length - 1, o = ''; i >= 0; o += s[i--]) {}
-        this.val = o;
+        this.val = reverse(this.val);
         return this;
     }
 
