58c4efcad3f7f5850ba202d0f10f9bc5a3225c56
chicagocomputerclasses <info@chicagocomputerclasses.com>
2016-01-11 12:45:23 -0600

    Update parts.js

--- parts.js
+++ parts.js
@@ -1,15 +1,5 @@
-var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\.\/:;<=>?@\[\]^_`{|}~]|(-\s)|(\s-)|(-[^a-zA-Z\d].)|([^a-zA-Z\d].-)/g;
 var spaceRE = /\s+/g;
-var str = "This, -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctu-ation re-enter";
+var str = "This - is- #! an $ % ^ & * -example ;: {} of a = -_ string- with `~)() -ddd punctu-ation re-enter";
 str.replace(punctRE, ' ').replace(spaceRE, ' ');
-"This is an example of a string with punctu ation re enter"
-
-
-var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\.\/:;<=>?@\[\]^_`{|}~]|(\s-\s)/g;
-var spaceRE = /\s+/g;
-var str = "This - is #! an $ % ^ & * example ;: {} of a = - _ string with `~)() punctu-ation re-enter";
-str.replace(punctRE, ' ').replace(spaceRE, ' ');
-"This is an example of a string with punctu-ation re-enter"
-
-
-
+"This is an example of a string with ddd punctu-ation re-enter"
