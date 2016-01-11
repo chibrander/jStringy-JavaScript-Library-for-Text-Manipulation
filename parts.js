eb5a6358bc7a103c9bd122cd6c445c4ac506d076
chicagocomputerclasses <info@chicagocomputerclasses.com>
2016-01-11 12:21:35 -0600

    Update parts.js

--- parts.js
+++ parts.js
@@ -5,4 +5,11 @@ str.replace(punctRE, ' ').replace(spaceRE, ' ');
 "This is an example of a string with punctu ation re enter"
 
 
+var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\.\/:;<=>?@\[\]^_`{|}~]|(\s-\s)/g;
+var spaceRE = /\s+/g;
+var str = "This - is #! an $ % ^ & * example ;: {} of a = - _ string with `~)() punctu-ation re-enter";
+str.replace(punctRE, ' ').replace(spaceRE, ' ');
+"This is an example of a string with punctu-ation re-enter"
+
+
 
