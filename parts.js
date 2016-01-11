0b867f2f42bf252ddf7d6c626537db3acbb04536
chicagocomputerclasses <info@chicagocomputerclasses.com>
2016-01-11 02:05:46 -0600

    Create parts.js

---
+++ parts.js
@@ -0,0 +1,8 @@
+var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+var spaceRE = /\s+/g;
+var str = "This, -/ is #! an $ % ^ & * example ;: {} of a = -_ string with `~)() punctu-ation re-enter";
+str.replace(punctRE, ' ').replace(spaceRE, ' ');
+"This is an example of a string with punctu ation re enter"
+
+
+
